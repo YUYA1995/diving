ca0692e1a29bc31ee8d8ce01a2b158a66d72486f
YUYA1995 <82224371+YUYA1995@users.noreply.github.com>
2021-08-24 14:17:10 +0900

    README編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'posts#index'
   get "posts/new"
-  resources :posts, only: [:index, :new, :create]
+  resources :posts, only: [:index, :new, :create, :edit, :update, :show]
 end
